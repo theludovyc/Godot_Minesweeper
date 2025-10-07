722d889f249eef7a3e935b3f21112442f90803f4
theludovyc <theludovyc@gmail.com>
2025-10-07 22:06:08 +0200

    update

--- game.gd
+++ game.gd
@@ -2,7 +2,7 @@ extends Node
 
 @onready var map = %Map
 @onready var flagLabel = %FoundMineLabel
-@onready var confirm_dialog:ConfirmationDialog = %ConfirmationDialog
+@onready var accept_dialog:AcceptDialog = %AcceptDialog
 
 const mapWidth = 10
 const maxMine = 15
@@ -14,9 +14,10 @@ func _ready() -> void:
 	
 	map.createMap(mapWidth, maxMine)
 	
-	confirm_dialog.confirmed.connect(_on_confirm_dialog_ok_pressed)
+	accept_dialog.confirmed.connect(_on_confirm_dialog_ok_pressed)
+	accept_dialog.canceled.connect(_on_confirm_dialog_ok_pressed)
 	
-	var confirm_dialog_label:Label = confirm_dialog.get_label()
+	var confirm_dialog_label:Label = accept_dialog.get_label()
 	confirm_dialog_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
 	confirm_dialog_label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
 
@@ -24,12 +25,12 @@ func _on_map_all_mines_flaged() -> void:
 	if flag_number > maxMine:
 		return
 
-	confirm_dialog.dialog_text = "Win !\nRetry ?"
-	confirm_dialog.show()
+	accept_dialog.dialog_text = "Win !\nRetry ?"
+	accept_dialog.show()
 
 func _on_map_mine_triggered() -> void:
-	confirm_dialog.dialog_text = "Game over !\nRetry ?"
-	confirm_dialog.show()
+	accept_dialog.dialog_text = "Game over !\nRetry ?"
+	accept_dialog.show()
 
 func _on_map_flag_number_updated(i: Variant) -> void:
 	flag_number = i
